2b8871b92d1267775b66f1bbdfd1503204c3a763
Vasiliy Yorkin <vyorkin@undev.ru>
2013-07-01 13:17:18 +0400

    add DocumentBuilder rspec tests

---
+++ spec/docxtor2/support/document_builder_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module Docxtor2
+  describe DocumentBuilder do
+    it 'should call known collaborators methods and return document' do
+      dsl_block = proc {}
+      document = double
+
+      DocumentBuilder.should_receive(:build).
+        with(&dsl_block).
+        and_return(document)
+
+      DocumentBuilder.build(&dsl_block)
+    end
+  end
+end
