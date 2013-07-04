b0bac56e5397b816d0b6e8fb740a93cd58bb938d
Vasiliy Yorkin <vyorkin@undev.ru>
2013-07-04 18:55:28 +0400

    add implementation for attributes processing in exist_xpath rspec matcher (only ideas, not tested, commented for now)

--- spec/docxtor2/support/matchers/exist_xpath.rb
+++ spec/docxtor2/support/matchers/exist_xpath.rb
@@ -4,6 +4,7 @@ module XPathMatchers
 
     EXPR_PREFIX = '//'
     LOCAL_NAME_EXPR = "*[local-name()='%s']"
+    ATTRIBUTE_SEPARATORS = ['|', '&', 'and', 'or']
 
     def initialize(expected)
       @expected = expected
@@ -21,20 +22,42 @@ module XPathMatchers
     end
 
     private 
+
+    # TODO: Write tests, use this instead of old version
     
-    # takes smth like //w:p/w:r and
-    # returns //*[local-name()='p']/*[local-name()='r]
+    # def self.to_local_name2(expr)
+    #   parts = expr.sub('*', '').split('[')
+    #   result = LOCAL_NAME_EXPR % parts.first.split(':').last
+    #   if parts.length > 1
+    #     attributes = parts[1][0..-1]
+    #     separators_regexp = Regexp.new(ATTRIBUTE_SEPARATORS.join)
+    #     attributes.split(separators_regexp).map { |token|
+    #       attributes.sub!(token, LOCAL_NAME_EXPR % token.split(':').last)
+    #     }
+    #     result += attributes
+    #   end
+    #   result
+    # end
 
     # TODO: Make it work with attributes
-    # //w:p/w:rPr/w:pStyle[@w:val='h1']
+    
+    # //person[@id='abc123']/@*[name()='weight' or name()='haircolor']
+    # //person[@id='abc123']/(@haircolor|@weight)`  
+
+    # [@id='abc123']
+    # @*[name()='weight' or name()='haircolor']
+
+    # takes smth like //w:p/w:r and
+    # returns //*[local-name()='p']/*[local-name()='r]
     def self.ignore_namespaces(xpath)
       expressions = xpath.sub(EXPR_PREFIX, '').
-        split('/').map { |expr|
-          element = expr.split(':').last
-          LOCAL_NAME_EXPR % element
-        }.join('/')
-
+        split('/').map { |expr| to_local_name(expr) }.join('/')
       EXPR_PREFIX + expressions
+    end
+
+    def self.to_local_name(expr)
+      element = expr.split(':').last
+      LOCAL_NAME_EXPR % element
     end
   end
 end
