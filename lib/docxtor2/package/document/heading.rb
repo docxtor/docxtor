f4c1d66513d5f5a5f426de3be87304b6142a31c7
Vasiliy Yorkin <vyorkin@undev.ru>
2013-07-08 21:35:42 +0400

    heading fix

--- lib/docxtor2/package/document/heading.rb
+++ lib/docxtor2/package/document/heading.rb
@@ -1,7 +1,11 @@
 module Docxtor2
   class Package::Document::Heading < Package::Document::Paragraph
-    def initialize(nesting = 1, &block)
-      super({ :style => nesting }, &block)
+    H1 = 1
+    H2 = 2
+    
+    def initialize(nesting = H1, *args, &block)
+      super(*args, &block)
+      @attrs[:style] ||= nesting
     end
   end
 end
